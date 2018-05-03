00cf114caa2e9687c933f0f0a71073f847a677d8
kouzi_prog <36578528+Y2Yamanaka@users.noreply.github.com>
2018-05-03 20:50:55 +0900

    Create switch.php

---
+++ switch.php
@@ -0,0 +1,14 @@
+<?php
+  $point = "B";
+  switch ($point){
+    case "A";
+      print("合格");
+      break;
+    case "B";
+      print("もうちょっと");
+      break;
+    case "C";
+      print("残念");
+      break;
+  }
+?>
