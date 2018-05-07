d4f46db0bce073239ea5df3ec7a1d750735adb25
kouzi_prog <36578528+Y2Yamanaka@users.noreply.github.com>
2018-05-07 20:03:32 +0900

    Create output.php

---
+++ output.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<title>output.php</title>
+	<meta charset="UTF-8">
+</head>	
+
+<body>
+	<?php
+	 $name = $_POST['name'];
+	 print("次のデータを受け取りました<br/>");
+	 print("名前:$name<br/>");
+	?> 
+</body>
+</html>	
